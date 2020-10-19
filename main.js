867b881a8c561991e7b27cc48ca66953b75c77f1
timegatekeeper <tim.pearce@bbc.co.uk>
2020-10-19 15:46:47 +0100

    added fetch bots from a json file. small refactor

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const canvasElement = document.querySelector('canvas');
 const context = canvasElement.getContext('2d');
-const botUrls = ['c/leftbot.wasm','c/randombot.wasm','c/sawtooth.wasm'];
 const height = 500;
 const width = 500;
 //let imageData = context.createImageData(width, height);
 let modules = [];
 let instances = [];
+let bots = [];
 let position = {
     x: 0,
     y: 0
 };
-
-let bots = [];
 
 class Location {
     constructor(x, y) {
@@ -43,24 +41,28 @@ class PaintBot {
     }
 };
 
+// TO DO maybe an application class
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
+async function getBotRoster(rosterUrl) {
+    const response = await fetch(rosterUrl);
+    return await response.json();
+}
+
 async function spawnBots(botUrls) {
+    let bots = [];
     for (const [idx, url] of botUrls.entries()) {
         const fetchPromise = fetch(url);
         const { module, instance } = await WebAssembly.instantiateStreaming(fetchPromise);
-        modules.push(module);
-        instances.push(instance);
         let spawnPoint = new Location(getRandomInt(width), getRandomInt(height));
         let bot = new PaintBot(spawnPoint, palette[idx], instance);
         bots.push(bot);
     }
-    requestAnimationFrame(update);
+    return bots;
 }
-
-spawnBots(botUrls);
 
 const vectorMovements = [
     [0,0],[0,-1],[1,-1],[1,0],[1,1],
@@ -74,17 +76,12 @@ function update(time) {
     requestAnimationFrame(update);
 }
 
-/*
-(async () => {
-    const fetchPromise = fetch('bot.wasm');
-    const { instance } = await WebAssembly.instantiateStreaming(fetchPromise, {});
-    //instance.exports.main();
-
-    function update(time) {
-        context.clearRect(0,0,canvasElement.width,canvasElement.height);
-        position = instance.exports.update(time);
-        context.fillRect(position*5,200,10,10);
-        requestAnimationFrame(update);
-    }
+async function run() {
+    let rosterUrl = "bots.json";
+    let botRoster = await getBotRoster(rosterUrl);
+    //TO DO: change to return modules and instances
+    bots = await spawnBots(botRoster);
     requestAnimationFrame(update);
-})();*/
+}
+
+run();
